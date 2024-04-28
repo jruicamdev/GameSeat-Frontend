64d7a236c06a53aaaa3df4bcae70e6c4bbeb9bb8
Javier Ruiz Camacho <javier.ruiz@oga.ai>
2024-04-28 16:14:51 +0200

    Cambio environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    api_url: "https://localhost:7220/",
-    //api_url:"poner-despliegue"
+    //api_url: "https://localhost:7220/",
+    api_url:"https://gameseat-0cf8f3de2b30.herokuapp.com/"
 };
